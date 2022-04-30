a64e59e64060df008f25477a2ec9ddb3b5f5fb62
신동민 <dongmyo@nhndooray.com>
2022-04-30 09:43:10 +0900

    * 실습 정답

--- src/main/java/com/nhnacademy/springmvc/controller/LoginController.java
+++ src/main/java/com/nhnacademy/springmvc/controller/LoginController.java
@@ -1,8 +1,18 @@
 package com.nhnacademy.springmvc.controller;
 
 import com.nhnacademy.springmvc.repository.UserRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-// TODO #1: Controller로 만드세요.
+import javax.servlet.http.*;
+
+@Controller
 public class LoginController {
     private final UserRepository userRepository;
 
@@ -10,21 +20,34 @@ public class LoginController {
         this.userRepository = userRepository;
     }
 
-
-    // TODO #2: `GET /login` 요청을 처리하세요.
-    //          `SESSION` 이라는 쿠키가 있으면 로그인 완료 메세지 출력 (`loginSuccess.jsp`).
-    //          `SESSION` 이라는 쿠키가 없으면 로그인 폼 화면 출력 (`loginForm.jsp`).
-    public String login() {
-        return null;
+    @GetMapping("/login")
+    public String login(@CookieValue(value = "SESSION", required = false) String session,
+                        Model model) {
+        if (StringUtils.hasText(session)) {
+            model.addAttribute("id", session);
+            return "loginSuccess";
+        } else {
+            return "loginForm";
+        }
     }
 
-    // TODO #3: `POST /login` 요청을 처리하세요.
-    //          `userRepository.matches(id, password)` 메서드 이용.
-    //          로그인 성공 시 `SESSION` 쿠키에 session id 값 저장하고
-    //          모델을 이용해서 `loginSuccess.jsp`에 세션 아이디 전달.
-    //          로그인 실패 시 `/login`으로 redirect.
-    public String doLogin() {
-        return null;
+    @PostMapping("/login")
+    public String doLogin(@RequestParam("id") String id,
+                          @RequestParam("pwd") String pwd,
+                          HttpServletRequest request,
+                          HttpServletResponse response,
+                          ModelMap modelMap) {
+        if (userRepository.matches(id, pwd)) {
+            HttpSession session = request.getSession(true);
+
+            Cookie cookie = new Cookie("SESSION", session.getId());
+            response.addCookie(cookie);
+
+            modelMap.put("id", session.getId());
+            return "loginSuccess";
+        } else {
+            return "redirect:/login";
+        }
     }
 
 }
