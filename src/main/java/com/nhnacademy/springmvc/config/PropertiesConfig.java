4b953f3e0f065e582fe08bfc73a04ab39e5e3243
신동민/NE <dongmyo@nhnent.com>
2022-05-04 02:37:05 +0900

    * `@Value` 활용

---
+++ src/main/java/com/nhnacademy/springmvc/config/PropertiesConfig.java
@@ -0,0 +1,26 @@
+package com.nhnacademy.springmvc.config;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+// TODO #2: Java Config 파일
+@Configuration
+@PropertySource("classpath:some.properties")
+public class PropertiesConfig {
+    @Value("${key1}")
+    private String key1;
+
+    @Bean
+    public List<String> keys(@Value("${key2}") String key2) {
+        List<String> list = new ArrayList<>();
+        list.add(key1);
+        list.add(key2);
+
+        return list;
+    }
+
+}
---
+++ src/main/java/com/nhnacademy/springmvc/controller/ValueTestController.java
@@ -0,0 +1,27 @@
+package com.nhnacademy.springmvc.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+// TODO #3: `@Value` 활용
+@Controller
+public class ValueTestController {
+    private final List<String> list;
+
+    public ValueTestController(List<String> list) {
+        this.list = list;
+    }
+
+    @GetMapping("/value-test")
+    @ResponseBody
+    public String valueTest(@Value("${key3}") String key3,
+                            @Value("12345") String key4,
+                            @Value("#{systemProperties['java.home']}") String javaHome) {
+        return String.join(",", list) + "," + key3 + "," + key4 + "\n"
+            + javaHome;
+    }
+
+}
