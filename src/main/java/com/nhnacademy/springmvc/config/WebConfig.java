601db58ce714b60fe4f6b049278a85730a6d5fa6
youaresogay22 <youaresogayee@gmail.com>
2024-02-20 11:09:47 +0900

    ViewController 실습

--- src/main/java/com/nhnacademy/springmvc/config/WebConfig.java
+++ src/main/java/com/nhnacademy/springmvc/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.nhnacademy.springmvc.controller.ControllerBase;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,5 +15,16 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         registry.jsp("/WEB-INF/view/", ".jsp");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        WebMvcConfigurer.super.addViewControllers(registry);
+
+        // /user2/register URL로 진입하면 userRegister로 리다이렉트
+        registry.addRedirectViewController("/user2/register", "userRegister");
+        
+        // /post2/register URL로 진입하면 userRegister로 리다이렉트
+        registry.addRedirectViewController("/post2/register", "postRegister");
     }
 }
