3955cecb38abdf6f5e73820a5824507e7ee47af3
youaresogay22 <youaresogayee@gmail.com>
2024-02-20 14:05:05 +0900

    HandlerInterceptor 실습 2-interceptor내부 변수 수정

--- src/main/java/com/nhnacademy/springmvc/interceptor/TimerInterceptor.java
+++ src/main/java/com/nhnacademy/springmvc/interceptor/TimerInterceptor.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.springmvc.interceptor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.StopWatch;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,19 +8,20 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Slf4j
 public class TimerInterceptor implements HandlerInterceptor {
-    StopWatch stopWatch = new StopWatch("controllertimer");
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        stopWatch.start();
+        request.setAttribute("start", System.currentTimeMillis());
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        stopWatch.stop();
-        stopWatch.prettyPrint();
+        long starttime = (long) request.getAttribute("start");
+        long endtime = System.currentTimeMillis();
+        log.info("timer: {}", endtime - starttime);
         HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
     }
 }
