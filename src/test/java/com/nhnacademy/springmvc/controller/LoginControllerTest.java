cba3d2f5d7b738a30f025e2b2e90667721e56464
youaresogay22 <youaresogayee@gmail.com>
2024-02-21 13:52:29 +0900

    Spring MVC Testing 실습

---
+++ src/test/java/com/nhnacademy/springmvc/controller/LoginControllerTest.java
@@ -0,0 +1,81 @@
+package com.nhnacademy.springmvc.controller;
+
+import com.nhnacademy.springmvc.domain.User;
+import com.nhnacademy.springmvc.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.Cookie;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+class LoginControllerTest {
+    private MockMvc mockMvc;
+
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() {
+        userRepository = mock(UserRepository.class);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(new LoginController(userRepository))
+                .build();
+    }
+
+    @Test
+    void testLogin() throws Exception {
+        final Cookie cookie = new Cookie("SESSION", "test");
+
+        mockMvc.perform(get("/login").cookie(cookie))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.model().attributeExists("id"))
+                .andExpect(view().name("loginSuccess"))
+                .andReturn();
+    }
+
+    @Test
+    void testLoginFail() throws Exception {
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("loginForm"))
+                .andReturn();
+    }
+
+    @Test
+    void testDoLogin() throws Exception {
+        when(userRepository.matches(anyString(), anyString())).thenReturn(true);
+
+        mockMvc.perform(post("/login")
+                        .param("id", "admin")
+                        .param("pwd", "12345"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("loginSuccess"))
+                .andReturn();
+
+    }
+
+    @Test
+    void testDoLoginFail() throws Exception {
+        when(userRepository.matches(anyString(), anyString())).thenReturn(false);
+
+        mockMvc.perform(post("/login")
+                        .param("id", "admin")
+                        .param("pwd", "12345"))
+                .andExpect(view().name("redirect:/login"))
+                .andReturn();
+    }
+}
